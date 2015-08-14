ebe7d809c7e67557db9eb8b3459ad23ac516d83e
jiunjiunma <jiunjiunma@gmail.com>
2015-08-14 14:37:25 -0700

    implement top 10 in spark

---
+++ src/main/scala/com/dataalgorithm/ch3/Top10.scala
@@ -0,0 +1,47 @@
+package com.dataalgorithm.ch3
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+ * Find the top 10 elements, will merge values of duplicate keys.
+ *
+ * @author jma
+ */
+object Top10 {
+  implicit val pairCompare = Ordering.by[(String, Int), Int](_._2)
+
+  def main(args: Array[String]): Unit = {
+    // $SPARK_HOME/bin/spark-submit --class "com.dataalgorithm.ch3.Top10"
+    // --master local[2] target/scala-2.11/data-algorithm-scala-assembly-1.0.jar
+    // file:///Users/jma/work/data-algorithm-scala/data/ch3/sample_input.txt
+    if (args.length < 1) {
+      System.err.println(s"Usage: Top10 <path>")
+      System.exit(1)
+    }
+    val path = args(0)
+    val conf = new SparkConf().setAppName("SecondarySort")
+    val sc = new SparkContext(conf)
+    val linesRDD = sc.textFile(path, 1)
+    val mappedRDD = linesRDD.map { line =>
+      val words = line.split("\\s")
+      (words(0), words(1).toInt)
+    }.reduceByKey(_+_)
+
+    mappedRDD.top(10).foreach(println)
+
+    // output
+    /*
+    (k10,88888)
+    (k8,9862)
+    (k12,988)
+    (k4,982)
+    (k5,333)
+    (k9,322)
+    (k3,88)
+    (k2,45)
+    (k1,23)
+    (k13,18)
+     */
+  }
+
+}
--- src/main/scala/com/dataalgorithm/ch3/Topic10Basic.scala
+++ src/main/scala/com/dataalgorithm/ch3/Topic10Basic.scala
@@ -10,8 +10,9 @@ object Topic10Basic {
   val testData = Map(("k1", 23), ("k2", 45), ("k3", 88), ("k4", 982), ("k5", 333), ("k6", 13), ("K7", 5),
     ("k8", 9862), ("k9", 322), ("k10", 88888), ("k11", 13), ("k12", 971), ("k13", 18))
 
-  // PairCompare has priority over the implicit val pairCompare
-  //implicit object PairCompare extends Ordering[(String, Int)] {
+  // Note: PairCompare has priority over the implicit val pairCompare
+
+  // implicit object PairCompare extends Ordering[(String, Int)] {
   //  override def compare(p1: (String, Int), p2: (String, Int)): Int = { println("xxx"); p2._2 compare p1._2 }
   //}
 
